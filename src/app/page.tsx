6e9e9ad8ce4a52958e292047768c9dc9168f661b
Ghaith <ghaithatrk93@gmail.com>
2024-03-13 23:20:25 +0800

    mobile view adjustment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import USDTBalance from "@/components/USDTBalance";
 
 export default function Home() {
   return (
-    <div className="h-screen bg-slate-600 p-20 flex flex-col gap-36 items-center">
+    <div className="h-screen bg-slate-600 p-4 md:p-20 flex flex-col gap-36 items-center">
       <Header />
       <LastBlock />
       <USDTBalance />
